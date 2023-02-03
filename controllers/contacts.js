9aa3271738b6e12a738f4c7e661e7872930bfe0a
indlukula <84289216+indlukula@users.noreply.github.com>
2023-02-03 16:51:57 +0200

    Added more roures

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -24,4 +24,66 @@ const getSingle = async (req, res, next) => {
   });
 };
 
-module.exports = { getAll, getSingle };
+const createContact = async (req, res) => {
+  const contact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday
+  };
+  (async ()=> {
+  const response = await mongodb.getDb().db().collection('contacts').insertOne(contact);
+  if (response.acknwledged) {
+    res.status(201).json(response);
+  } else {
+    res.status(500).json(response.error || 'Some error occured while creating the contact.');
+  }
+})()
+};
+
+  const updateContact = async (req, res) => {
+    const userId = new ObjectId(req.params.id);
+    // be aware of updateOne if you only want to update specific fields
+    const contact = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      favoriteColor: req.body.favoriteColor,
+      birthday: req.body.birthday
+    }
+    };
+
+(async()=> {
+  const response = await mongodb
+  .getDb()
+  .db()
+  .collection('contacts')
+  .replaceOne({ _id: userId }, contact);
+  console.log(response);
+  if (response.modifiedCount > 0) {
+  res.status(204).send();
+  } else {
+  res.status(500).json(response.error || 'Some error occurred while updating the contact.');
+  }
+})()
+
+const deleteContact = async (req, res) => {
+  const userId = new ObjectId(req.params.id);
+  const response = await mongodb.getDb().db().collection('contacts').remove({ _id: userId }, true);
+  console.log(response);
+  if (response.deletedCount > 0) {
+    res.status(204).send();
+  } else {
+    res.status(500).json(response.error || 'Some error occurred while deleting the contact.');
+  }
+};
+
+
+module.exports = { 
+  getAll, 
+  getSingle,
+  createContact,
+  updateContact,
+  deleteContact
+ };
