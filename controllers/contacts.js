f2c98f8315ffba295966e3a00aeaa8d79a089a4b
indlukula <84289216+indlukula@users.noreply.github.com>
2023-02-03 18:50:47 +0200

    Added eslintrc.json and prettierrc

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -32,25 +32,25 @@ const createContact = async (req, res) => {
     favoriteColor: req.body.favoriteColor,
     birthday: req.body.birthday
   };
-  (async ()=> {
+  //(async ()=> {
   const response = await mongodb.getDb().db().collection('contacts').insertOne(contact);
   if (response.acknwledged) {
     res.status(201).json(response);
   } else {
     res.status(500).json(response.error || 'Some error occured while creating the contact.');
   }
-})()
+//})()
 };
 
-  const updateContact = async (req, res) => {
-    const userId = new ObjectId(req.params.id);
+const updateContact = async (req, res) => {
+  const userId = new ObjectId(req.params.id);
     // be aware of updateOne if you only want to update specific fields
-    const contact = {
-      firstName: req.body.firstName,
-      lastName: req.body.lastName,
-      email: req.body.email,
-      favoriteColor: req.body.favoriteColor,
-      birthday: req.body.birthday
+  const contact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday
     }
     };
 
@@ -66,7 +66,7 @@ const createContact = async (req, res) => {
   } else {
   res.status(500).json(response.error || 'Some error occurred while updating the contact.');
   }
-})()
+});
 
 const deleteContact = async (req, res) => {
   const userId = new ObjectId(req.params.id);
